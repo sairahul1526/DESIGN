a6f995cfc4301b2ee1956606378f2ad924ee2ee7
sairahul1526 <dravid.rahul1526@gmail.com>
2016-03-26 22:28:58 +0530

    Initial Commit

---
+++ DESIGN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DESIGN
+//
+//  Created by Sai Krishna on 26/03/16.
+//  Copyright © 2016 Sai Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
